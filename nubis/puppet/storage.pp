5935399f1b4a1da6fa6cbc9590f02fb0aa972847
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-09-04 12:31:34 -0400

    temporary fix until puppet-nubis-storage is fixed

--- nubis/puppet/storage.pp
+++ nubis/puppet/storage.pp
@@ -1,6 +1,19 @@
 include nubis_storage
 nubis::storage { "$project_name": }
 
+### This should be in puppet-nubis-storage
+package { "ceph":
+  ensure => present,
+}
+
+# need to fix #! to use python26
+exec { "fix-ceph-shebang":
+  command => "sed -i -e '1c#!/usr/bin/env python26' /usr/bin/ceph*",
+  require => Package["ceph"],
+}
+
+### puppet-nubis-storage
+
 # Link to our mountpoint
 file { "/var/www/bugzilla/data":
   ensure => 'link',
