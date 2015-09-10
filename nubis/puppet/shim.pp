0768461dff253ab6de8fa3b33519ad617652623e
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-09-10 12:18:49 -0400

    checkout SHIM code and dependencies

--- nubis/puppet/shim.pp
+++ nubis/puppet/shim.pp
@@ -1,2 +1,13 @@
 class { 'supervisord':
 }
+
+package { "MySQL-python":
+  ensure => present,
+}
+
+vcsrepo { "/opt/pulseshims":
+  ensure   => present,
+  provider => "mercurial",
+  source   => 'https://hg.mozilla.org/automation/pulseshims',
+  revision => "f8fc683ea85e",
+}
