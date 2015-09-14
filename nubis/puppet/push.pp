785e70a668b65655c7ddce1cb2f08ec1ce2dd629
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-09-14 13:27:07 -0400

    tyop

--- nubis/puppet/push.pp
+++ nubis/puppet/push.pp
@@ -1,6 +1,6 @@
 file { "/etc/init.d/bugzilla-push":
   ensure => present,
-  source => "puppet:///nubis/files/bugzilla-push.ini",
+  source => "puppet:///nubis/files/bugzilla-push.init",
   owner  => 'root',
   group  => 'root',
   mode   => '0755',
