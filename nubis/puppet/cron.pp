0b87d1806e1a9ce7a46fc6cb4200e89223001788
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-09-25 12:21:42 -0400

    send cron output to syslog

--- nubis/puppet/cron.pp
+++ nubis/puppet/cron.pp
@@ -1,6 +1,6 @@
 cron { 'data-sync':
   ensure => 'present',
-  command => "consul-do bugzilla-cron-data-sync $(hostname) && /usr/local/bin/bugzilla-data-sync",
+  command => "consul-do bugzilla-cron-data-sync $(hostname) && /usr/local/bin/bugzilla-data-sync 2>&1 | logger -t bugzilla-cron-data-sync",
   hour => '*',
   minute => '30',
   user => 'root',
@@ -11,7 +11,7 @@ cron { 'data-sync':
 
 cron { 'collectstats':
   ensure => 'present',
-  command => "cd /var/www/bugzilla && consul-do bugzilla-cron-collectstats $(hostname) && perl -Mlib=lib collectstats.pl",
+  command => "cd /var/www/bugzilla && consul-do bugzilla-cron-collectstats $(hostname) && perl -Mlib=lib collectstats.pl 2>&1 | logger -t bugzilla-cron-collectstats",
   hour => '0',
   minute => '0',
   user => 'root',
@@ -22,7 +22,7 @@ cron { 'collectstats':
 
 cron { 'moco-ldap-check':
   ensure => 'present',
-  command => "consul-do bugzilla-cron-moco-ldap-check $(hostname) && bugzilla-run-if-active /usr/local/bin/bugzilla-moco-ldap-check -cron -email",
+  command => "consul-do bugzilla-cron-moco-ldap-check $(hostname) && bugzilla-run-if-active /usr/local/bin/bugzilla-moco-ldap-check -cron -email 2>&1 | logger -t bugzilla-cron-moco-ldap-check",
   hour => '0',
   minute => '21',
   user => 'root',
@@ -33,7 +33,7 @@ cron { 'moco-ldap-check':
 
 cron { 'whine':
   ensure => 'present',
-  command => "cd /var/www/bugzilla && consul-do bugzilla-cron-whine $(hostname) && bugzilla-run-if-active perl -Mlib=lib whine.pl",
+  command => "cd /var/www/bugzilla && consul-do bugzilla-cron-whine $(hostname) && bugzilla-run-if-active perl -Mlib=lib whine.pl 2>&1 | logger -t bugzilla-cron-whine",
   minute => '*/15',
   user => 'apache',
   environment => [
@@ -43,7 +43,7 @@ cron { 'whine':
 
 cron { 'prune-last-visit':
   ensure => 'present',
-  command => "cd /var/www/bugzilla && consul-do bugzilla-cron-prune-last-visit $(hostname) && bugzilla-run-if-active perl -Mlib=lib clean-bug-user-last-visit.pl",
+  command => "cd /var/www/bugzilla && consul-do bugzilla-cron-prune-last-visit $(hostname) && bugzilla-run-if-active perl -Mlib=lib clean-bug-user-last-visit.pl 2>&1 | logger -t bugzilla-cron-prune-last-visit",
   hour => '0',
   minute => '0',
   user => 'apache',
@@ -54,7 +54,7 @@ cron { 'prune-last-visit':
 
 cron { 'requestnagger':
   ensure => 'present',
-  command => "cd /var/www/bugzilla && consul-do bugzilla-cron-requestnagger $(hostname) && bugzilla-run-if-active perl -Mlib=lib extensions/RequestNagger/bin/send-request-nags.pl",
+  command => "cd /var/www/bugzilla && consul-do bugzilla-cron-requestnagger $(hostname) && bugzilla-run-if-active perl -Mlib=lib extensions/RequestNagger/bin/send-request-nags.pl 2>&1 | logger -t bugzilla-cron-requestnagger",
   hour => '0',
   minute => '30',
   user => 'apache',
@@ -65,7 +65,7 @@ cron { 'requestnagger':
 
 cron { 'userprofile':
   ensure => 'present',
-  command => "cd /var/www/bugzilla && consul-do bugzilla-cron-userprofile $(hostname) && bugzilla-run-if-active perl -Mlib=lib extensions/UserProfile/bin/update.pl",
+  command => "cd /var/www/bugzilla && consul-do bugzilla-cron-userprofile $(hostname) && bugzilla-run-if-active perl -Mlib=lib extensions/UserProfile/bin/update.pl 2>&1 | logger -t bugzilla-cron-userprofile",
   hour => '0',
   minute => '30',
   user => 'apache',
