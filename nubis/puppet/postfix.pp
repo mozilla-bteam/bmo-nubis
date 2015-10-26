6c0af96e6b33b62520e79b0cec59de5c92a4749c
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-10-26 15:55:33 -0400

    Global Postifx settings for relaying

---
+++ nubis/puppet/postfix.pp
@@ -0,0 +1,23 @@
+# Global settings to enable SES
+
+postfix::config::maincfhelper { "smtp_use_tls":
+  value => "yes"
+}
+postfix::config::maincfhelper { "smtp_tls_security_level":
+  value => "encrypt"
+}
+postfix::config::maincfhelper { "smtp_tls_note_starttls_offer":
+  value => "yes"
+}
+postfix::config::maincfhelper { "smtp_tls_CAfile":
+  value => "/etc/ssl/certs/ca-bundle.crt"
+}
+postfix::config::maincfhelper { "smtp_sasl_auth_enable":
+  value => "yes"
+}
+postfix::config::maincfhelper { "smtp_sasl_security_options":
+  value => "noanonymous"
+}
+postfix::config::maincfhelper { "smtp_sasl_password_maps":
+  value => "hash:/etc/postfix/sasl_passwd"
+}
