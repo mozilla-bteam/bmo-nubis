9d1e7ee01898920789f61daeb937db0a612dab7e
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-11-19 21:42:08 -0500

    send queue size metrics to cloudwatch

--- nubis/puppet/queue.pp
+++ nubis/puppet/queue.pp
@@ -6,3 +6,22 @@ file { "/etc/init.d/bugzilla-queue":
   mode   => '0755',
 }
 
+file { "/usr/local/bin/bugzilla-cloudwatch-queue-size":
+  ensure => present,
+  source => "puppet:///nubis/files/cloudwatch-queue-size",
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0755',
+}
+
+cron { 'cloudwatch-queue-size':
+  ensure => 'present',
+  command => "consul-do bugzilla-cloudwatch-queue-size $(hostname) && /usr/local/bin/bugzilla-cloudwatch-queue-size 2>&1 | logger -t bugzilla-cloudwatch-queue-size",
+  hour => '*',
+  minute => '*',
+  user => 'root',
+  environment => [
+    "PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:/opt/aws/bin",
+  ],
+}
+
