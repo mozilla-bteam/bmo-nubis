8bd08ee811007f1ee716e529edadf69049b8992e
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-12-30 13:11:40 -0500

    build against Nubis v1.3.0

--- nubis/puppet/storage.pp
+++ nubis/puppet/storage.pp
@@ -1,26 +1,9 @@
 include nubis_storage
-nubis::storage { "$project_name": }
 
-yumrepo { 'ceph':
-  descr    => 'Ceph',
-  baseurl  => "http://download.ceph.com/rpm-hammer/el6/x86_64",
-  priority => 2,
-  enabled  => 1,
-  gpgcheck => 1,
-  gpgkey   => 'https://download.ceph.com/keys/release.asc',
-}
-
-### This should be in puppet-nubis-storage
-package { "ceph":
-  ensure => latest,
-  require => Yumrepo['ceph'],
-}
-
-# need to fix #! to use python26
-exec { "fix-ceph-shebang":
-  command => "file /usr/bin/ceph* | grep -i 'Python script' | cut -d: -f1 | xargs sed -i -e '1c#!/usr/bin/env python26'",
-  require => Package["ceph"],
-  path => ['/sbin','/bin','/usr/sbin','/usr/bin','/usr/local/sbin','/usr/local/bin'],
+nubis::storage { "$project_name":
+  type => "efs",
+  owner => "apache",
+  group => "apache",
 }
 
 ### puppet-nubis-storage
